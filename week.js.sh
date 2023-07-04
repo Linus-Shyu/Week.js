231a48e0a4b53c9f77fe0736c6120eb4af9d5d56
Linus Shyu <83543818+Linus-Shyu@users.noreply.github.com>
2023-07-04 22:06:39 +0800

    Add files via upload

---
+++ week.js.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+echo -e "\033[32mVersion 1.0.0🎉"
+echo -e "\033[32mWriter: Ether Guy"
+
+echo -e "\033[32mThis is Week.js my friend!"
+
+echo -e '\033[32m                                   /$$                        '
+echo -e '\033[32m                                  | $$                        '
+echo -e '\033[32m /$$  /$$  /$$  /$$$$$$   /$$$$$$ | $$   /$$     /$$  /$$$$$$$'
+echo -e '\033[32m| $$ | $$ | $$ /$$__  $$ /$$__  $$| $$  /$$/    |__/ /$$_____/'
+echo -e '\033[32m| $$ | $$ | $$| $$$$$$$$| $$$$$$$$| $$$$$$/      /$$|  $$$$$$ '
+echo -e '\033[32m| $$ | $$ | $$| $$_____/| $$_____/| $$_  $$     | $$ \____  $$'
+echo -e '\033[32m|  $$$$$/$$$$/|  $$$$$$$|  $$$$$$$| $$ \  $$ /$$| $$ /$$$$$$$/'
+echo -e '\033[32m \_____/\___/  \_______/ \_______/|__/  \__/|__/| $$|_______/ '
+echo -e '\033[32m                                           /$$  | $$          '
+echo -e '\033[32m                                          |  $$$$$$/          '
+echo -e '\033[32m                                           \______/           '
+
+mkdir project
+
+cd project
+
+touch index.html
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>' > index.html
+touch index.css
+touch index.js
+echo 'import {ethers} from "ethers";' > index.js
+
+npm install ethers@5.6.9
+
+echo '{
+  "type": "module",
+  "name": "ethers",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC"
+}' > package.json
+
+echo "See you next develop time!"
+
+
+
